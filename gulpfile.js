a37881d39202ba898343d56eb5877e5cd292d3b9
小楼 <xuolou@163.com>
2023-11-08 00:48:56 +0800

    Update the project structure.

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,69 @@
 import webserver from 'gulp-webserver'
 import fs from 'fs-extra'
-import pkg from 'gulp'
+import clean from 'gulp-clean'
+import gulp from 'gulp'
 import minimist from 'minimist'
-import {exec} from 'child_process'
-const {src} = pkg
-const i18nMap = new Map([
-  ["zh","gitee"],
-  ["en","github"]
-])
-const ServeURL = './src/assets/serve.json'
 
-async function simplify(){
-  let npm=await fs.readJSON('package.json')
-  let serve=await fs.readJSON(ServeURL)
-  let options = minimist(process.argv.slice(2), {string: 'host', default: ''})
-  /* change package.json */
-  delProp(serve,npm)
-  function delProp(obj,target){
-    for(let i in obj){
-      let item=obj[i]
-      if(Array.isArray(item)&&target[i]){
-        for(let child of item){
-          let index=target[i].findIndex(str=>str===child)
-          if(index!==-1)target[i].splice(index,1)
-        }
-      }else if(typeof(item)==='object'&&target[i]&&item!==null){
-        delProp(item,target[i])
-      }else{
-        Reflect.deleteProperty(target,i)
-      }
-    }
+const {src} = gulp
+const languages = ["zh", "ja", "ko", "fr"]
+
+const clear = function() {
+  return src(["dist/es/*", "dist/umd/*", "index.d.ts"], {
+    read: false,
+    allowEmpty: true
+  }).pipe(clean())
+}
+
+const serve = function() {
+  return src('./').pipe(webserver({
+    host: '127.0.0.1',
+    port: '5000',
+    livereload: true,
+    open: 'docs/index.html'
+  }))
+}
+
+async function upgrade() {
+  const options = minimist(process.argv.slice(2), { string: 'host', default: '' })
+  const pkg = await fs.readJSON('package.json')
+  let { version, name } = pkg
+  if (/^\d+\.\d+\.\d+$/.test(options.host)) {
+    version = options.host
+  } else {
+    version = upgradeVersion(version)
   }
-  /* update version */
-  if(options.host==="upgrade")npm.version=updateVersion(npm.version)
-  function updateVersion(str){
-    let arr=str.split(".").map(item=>Number(item))
-    let thr=arr[2]+1
-    if(thr>=100){
-      let two=arr[1]+1
-      if(two>=100){
-        arr[0]=arr[0]+1
-        arr[1]=arr[2]=0
-      }else{
-        arr[1]=two
-        arr[2]=0
+  pkg.version = version
+  await fs.writeJSON('package.json', pkg, { spaces: 2 })
+  upgradeFile("readme.md")
+  languages.forEach(item => {
+    upgradeFile(`docs/md/readme-${item}.md`)
+  })
+
+  async function upgradeFile(url) {
+    let text = await fs.readFile(url, "utf8")
+    text = text.replace(new RegExp(`\/${name}@\\d+\\.\\d+\\.\\d+\/`, "g"), `/${name}@${version}/`)
+    await fs.writeFile(url, text)
+  }
+
+  function upgradeVersion(str) {
+    let arr = str.split(".").map(Number)
+    let thr = arr[2] + 1
+    if (thr >= 100) {
+      let two = arr[1] + 1
+      if (two >= 100) {
+        arr[0] = arr[0] + 1
+        arr[1] = arr[2] = 0
+      } else {
+        arr[1] = two
+        arr[2] = 0
       }
-    }else{
-      arr[2]=thr
+    } else {
+      arr[2] = thr
     }
     return arr.join(".")
   }
-  /* update readme.md */
-  await i18n("en")
-  await fs.writeJSON('package.json',npm,{spaces:2})
 }
 
-async function dev(){
-  const npm=await fs.readJSON('package.json')
-  const serve=await fs.readJSON(ServeURL)
-  addProp(serve,npm)
-  function addProp(obj,target){
-    for(let i in obj){
-      let item=obj[i]
-      if(Array.isArray(item)&&target[i]){
-        target[i]=target[i].concat(item)
-      }else if(typeof(item)==='object'&&target[i]&&item!==null){
-        addProp(item,target[i])
-      }else{
-        target[i]=item
-      }
-    }
-  }
-  await fs.writeJSON('package.json',npm,{spaces:2})
-}
-
-async function push(){
-  let options = minimist(process.argv.slice(2), {
-    string: ['host', 'port'],
-    default: {host: "en", port: ""}
-  })
-  if(! i18nMap.has(options.host)) options.host = "en"
-  await i18n(options.host)
-  execFunc(`git add -A && git commit -m "${options.port}" && git push ${i18nMap.get(options.host)} master`)
-}
-
-async function i18n(key){
-  let options = key? {host: key} : minimist(process.argv.slice(2), {string: 'host', default: 'en'});
-  if(! i18nMap.has(options.host)) options.host = "en"
-  let i18n = await fs.readFile(`./src/assets/i18n-${options.host}.md`, 'utf8')
-  let {version} = await fs.readJSON('package.json')
-  i18n = i18n.replace(/\/simulate@\d+\.\d+\.\d+\//g, `/simulate@${version}/`)
-  await fs.writeFile('readme.md', i18n)
-  return options.host
-}
-
-function execFunc(str){
-  exec(str, function(err, stdout, stderr){
-    if(err === null){
-      console.log("Command executed successfully!")
-    }else{
-      console.log(stdout, stderr)
-    }
-  })
-}
-
-function serve(){
-  return src('./').pipe(webserver({
-    host:'127.0.0.1',
-    port:'5000',
-    livereload:true,
-    open:'docs/index.html'
-  }))
-}
 export {
-  serve,simplify,dev,i18n,push
+  clear, serve, upgrade
 }
--- rollup.config.js
+++ rollup.config.js
@@ -3,64 +3,60 @@ import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from "@rollup/plugin-json"
+import dts from 'rollup-plugin-dts'
 
-const name="simulate"
-const globalName="Simulate"
-const {NODE_ENV}=process.env
-const commonPlugins=[
+const name = "simulate"
+const globalName = "Simulate"
+const commonPlugins = [
   resolve(),
   commonjs(),
   json(),
   typescript()
 ]
 
-const config=[
+const config = [
   {
-    input:"src/main.ts",
+    input: "src/main.umd.ts",
     output:[
       {
-        file:`dist/umd/${name}.js`,
-        format:'umd',
-        name:globalName,
+        file: `dist/umd/${name}.js`,
+        format: 'umd',
+        name: globalName,
       },
       {
-        file:`dist/umd/${name}.min.js`,
-        format:'umd',
-        name:globalName,
-        sourcemap:true,
+        file: `dist/umd/${name}.min.js`,
+        format: 'umd',
+        name: globalName,
+        sourcemap: true,
         plugins: [terser()]
       },
+    ],
+    plugins: commonPlugins
+  },
+  {
+    input: "src/main.ts",
+    output: [
       {
-        file:`dist/es/${name}.js`,
-        format:'es'
+        file: `dist/es/${name}.js`,
+        format: 'es'
       },
       {
-        file:`dist/es/${name}.min.js`,
-        format:'es',
-        sourcemap:true,
+        file: `dist/es/${name}.min.js`,
+        format: 'es',
+        sourcemap: true,
         plugins: [terser()]
       }
     ],
     plugins: commonPlugins
+  },
+  {
+    input: "src/main.ts",
+    output: {
+      file: 'index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()]
   }
 ]
-if(NODE_ENV==="development"){
-  config.push({
-    input:"src/app.ts",
-    output:[
-      {
-        file:`docs/app.js`,
-        format:'umd'
-      },
-      {
-        file:`docs/app.min.js`,
-        format:'umd',
-        sourcemap:true,
-        plugins: [terser()]
-      }
-    ],
-    plugins: commonPlugins
-  })
-}
 
 export default config
